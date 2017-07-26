a0c74987cdda0b5f151a95d9e7ee20614bb070c1
Amirhossein Naghshzan <n.naghshzan@gmail.com>
2017-07-26 10:01:45 +0430

    Final changes

--- sample/src/main/java/com/amirarcane/sample/MainActivity.java
+++ sample/src/main/java/com/amirarcane/sample/MainActivity.java
@@ -10,17 +10,57 @@ import com.amirarcane.lockscreen.activity.EnterPinActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String FONT_TEXT = "font/ALEAWB.TTF";
+    public static final String FONT_NUMBER = "font/BLKCHCRY.TTF";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button);
+        Button normal = (Button) findViewById(R.id.normal);
+        Button setPin = (Button) findViewById(R.id.setPin);
+        Button setFont = (Button) findViewById(R.id.setFont);
+        Button setPinAndFont = (Button) findViewById(R.id.setPinAndFont);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        normal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                // start the activity, It handles the setting and checking
                 Intent intent = new Intent(MainActivity.this, EnterPinActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        setPin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // set pin instead of checking it
+                Intent intent = EnterPinActivity.getIntent(MainActivity.this, true);
+                startActivity(intent);
+
+            }
+        });
+
+        setFont.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // setting font for library
+                Intent intent = EnterPinActivity.getIntent(MainActivity.this, FONT_TEXT, FONT_NUMBER);
+                startActivity(intent);
+            }
+        });
+
+        setPinAndFont.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // setting font for library and set pin instead of checking it
+                Intent intent = EnterPinActivity.getIntent(MainActivity.this, true, FONT_TEXT, FONT_NUMBER);
                 startActivity(intent);
             }
         });
